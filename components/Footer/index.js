0294353b1d928206c0af8145100724f5e0190010
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2020-11-18 21:44:40 +0000

    removed cv icon

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,7 +6,6 @@ import styles from './Footer.module.css'
 
 const Header = () => (
     <footer className={styles.footer}>
-        <a href="/public/CVJack_Burgess.pdf" download><FontAwesomeIcon className={styles.icon} icon={faGithubSquare} size='5x'/></a>
         <a href="https://www.linkedin.com/in/jackcburgess/"><FontAwesomeIcon className={styles.icon} icon={faLinkedin} size='5x'/></a>
         <a href="https://github.com/jackthb"><FontAwesomeIcon className={styles.icon} icon={faGithubSquare} size='5x'/></a>
         <a href="https://www.twitter.com/jack_thb"><FontAwesomeIcon className={styles.icon} icon={faTwitterSquare} size='5x'/></a>
