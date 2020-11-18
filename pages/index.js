fdd33d9ed4683f05c174204119f70668a2e7a3c9
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2020-11-18 22:38:42 +0000

    bakground

--- pages/index.js
+++ pages/index.js
@@ -17,42 +17,8 @@ export default function Home() {
       <main>
         <Header />
         <TempCard />
-{/*         
-        <div className='intro'>
-          <div className='left'>
-            <h1 className="title">
-              Jack C. Burgess
-            </h1>
-            <h2 className="intro">
-              Computer Science undergrad and software developer
-            </h2>
-          </div>
-          <div className='right'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ligula ligula, auctor efficitur ullamcorper a, ultricies eget enim.
-          </div>
-        </div>
-        <h1 className="pages">
-          Read <Link href="/projects/projects"><a>this page!</a></Link>
-        </h1> */}
       <Footer/>
       </main>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: 
-          background-color: white;
-            -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
