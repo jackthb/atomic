3485c3e9e24cee96d386be3ef5a3ab3bc376fe65
jackthb <19895932+jackthb@users.noreply.github.com>
2020-12-13 19:25:26 +0000

    fade in items

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -9,7 +9,7 @@ const Header = () => (
         <a href="https://www.linkedin.com/in/jackcburgess/"><FontAwesomeIcon className={styles.icon} icon={faLinkedin} size='5x'/></a>
         <a href="https://github.com/jackthb"><FontAwesomeIcon className={styles.icon} icon={faGithubSquare} size='5x'/></a>
         <a href="https://www.twitter.com/jack_thb"><FontAwesomeIcon className={styles.icon} icon={faTwitterSquare} size='5x'/></a>
-    </footer>  
+    </footer>
 )
 
 export default Header;
