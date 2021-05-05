8823ebdbd3fb6f8050d2da24950a41c41c8227ef
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-05 14:16:25 +0100

    fixing book component

--- components/Book.js
+++ components/Book.js
@@ -1,17 +1,9 @@
 import React, { useEffect } from 'react';
 
-export default function Book({ props }) {
-  const { data, error } = useSWR(
-    'https://get-current-book-goodreads.herokuapp.com/api/getcurrentbook',
-    fetcher
-  );
-
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
-
+export default function Book({ title, author }) {
   return (
-    <h1 className='text-2xl font-bold w-80 mx-12 rounded border-1 border-t-4 border-b-4 border- border-yellow-500 p-4'>
-      I am currently reading {data.title}!
+    <h1 className='text-2xl font-bold mx-12 rounded border-1 border-t-4 border-b-4 border- border-yellow-500 p-4'>
+      I am currently reading {title} by {author}!
     </h1>
   );
 }
