819aad67d59b8166e141d3f01119ac5b7c6fbb8d
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-11 01:42:50 +0100

    no maxwidth with tailwind on deploy?

--- components/Book.js
+++ components/Book.js
@@ -1,7 +1,7 @@
 export default function Book({ book }) {
   const { title, author, image } = book;
   return (
-    <div className="max-w-lg mx-auto flex rounded border-1 border-t-4 border-b-4 border-yellow-500 p-4">
+    <div className="sm:max-w-lg mx-auto flex rounded border-1 border-t-4 border-b-4 border-yellow-500 p-4">
       <img draggable={false} src={image} className="w-24 h-auto select-none" />
       <div className="flex flex-col px-5">
         <h1 className="text-1xl">Currently reading</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  corePlugins: {
+    // ...
+    maxWidth: true,
+  },
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      display: ['Montserrat'],
-      sans: ['Lato', 'sans-serif'],
-      mono:
-        'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+      display: ["Montserrat"],
+      sans: ["Lato", "sans-serif"],
+      mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
     },
   },
   variants: {
