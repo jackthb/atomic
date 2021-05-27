ffc86c67733e39befec9d7348305296c3fae398c
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-27 18:13:50 +0100

    cleaning and adding custom sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 module.exports = {
-  corePlugins: {
-    // ...
-    maxWidth: true,
-  },
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      display: ["Montserrat"],
-      sans: ["Lato", "sans-serif"],
-      mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	corePlugins: {
+		// ...
+		maxWidth: true,
+	},
+	purge: ["./pages/**/*.js", "./components/**/*.js"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			display: ["Montserrat"],
+			sans: ["Lato", "sans-serif"],
+			mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+		},
+		extend: {
+			height: {
+				header: "7rem",
+				body: "968px",
+			},
+		},
+	},
 };
