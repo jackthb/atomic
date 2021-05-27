9f4f04ba82ae881d963beae758f2a1a63168b07b
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-27 18:15:08 +0100

    styling and revalidate

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,24 @@
-import '../public/styles.css';
-import '../public/nprogress.css';
-import 'react-notion/src/styles.css';
+import "../public/styles.css";
+import "../public/nprogress.css";
+import "react-notion/src/styles.css";
 
-import React from 'react';
+import React from "react";
 
-import Layout from '../components/layout';
+import Layout from "../components/Layout";
 
-import NProgress from 'nprogress';
-import Router from 'next/router';
+import NProgress from "nprogress";
+import Router from "next/router";
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
-  );
+	return (
+		<>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	);
 }
