246980928db72216fe20b2ddbce92decddf862a7
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-10-30 11:55:51 +0100

    ^^

--- components/Header.js
+++ components/Header.js
@@ -2,27 +2,27 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Header() {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<header>
-			<div className="py-0 px-5 sm:text-xl text-sm onLoad max-w-6xl m-auto h-28 flex items-center font-display">
-				<div className={`${router.pathname == "/" ? "active" : ""} title`}>
-					<Link className="" href="/">
-						JACK BURGESS
-					</Link>
-				</div>
-				<nav className="block ml-auto">
-					<Link href="/about">
-						<a
-							className={`${
-								router.pathname == "/about" ? "about" : ""
-							} title mr-4`}
-						>
-							About
-						</a>
-					</Link>
-					<Link href="/projects">
+  return (
+    <header>
+      <div className="py-0 px-5 sm:text-xl text-sm onLoad max-w-6xl m-auto h-28 flex items-center font-display">
+        <div className={`${router.pathname == "/" ? "active" : ""} title`}>
+          <Link className="" href="/">
+            JACK BURGESS
+          </Link>
+        </div>
+        <nav className="block ml-auto">
+          <Link href="/about">
+            <a
+              className={`${
+                router.pathname == "/about" ? "about" : ""
+              } title mr-4`}
+            >
+              About
+            </a>
+          </Link>
+          {/* <Link href="/projects">
 						<a
 							className={`${
 								router.pathname == "/projects" ? "projects" : ""
@@ -30,8 +30,8 @@ export default function Header() {
 						>
 							Projects
 						</a>
-					</Link>
-					{/* <Link href='/blog'>
+					</Link> */}
+          {/* <Link href='/blog'>
             <a
               className={`${
                 router.pathname == '/blog' ? 'blog' : ''
@@ -40,11 +40,11 @@ export default function Header() {
               Blog
             </a>
           </Link> */}
-					<a href="mailto:jackcburgess@gmail.com" className="title">
-						Contact
-					</a>
-				</nav>
-			</div>
-		</header>
-	);
+          <a href="mailto:jackcburgess@gmail.com" className="title">
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,53 +1,56 @@
 import Link from "next/link";
 import Head from "next/head";
-const NOTION_BLOG_ID =
-	"7520df31ba244264ab074da9129a0fc5" || process.env.NOTION_BLOG_ID;
 
-export const getAllPosts = async () => {
-	return await fetch(
-		`https://notion-worker.jackcburgess.workers.dev/v1/table/${NOTION_BLOG_ID}`
-	).then((res) => res.json());
-};
+import { getAllPosts } from "../api/notion";
+const NOTION_BLOG_ID =
+  process.env.NOTION_BLOG_ID ?? "7520df31ba244264ab074da9129a0fc5";
+
+// export const getAllPosts = async () => {
+//   return await fetch(
+//     `https://notion-worker.jackcburgess.workers.dev/v1/table/${NOTION_BLOG_ID}`
+//   ).then((res) => res.json());
+// };
+
 export async function getStaticProps() {
-	const posts = await getAllPosts();
-	return {
-		props: {
-			posts,
-		},
-		revalidate: 1,
-	};
+  const posts = await getAllPosts();
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 1,
+  };
 }
 
 export default function HomePage({ posts }) {
-	return (
-		<div className="p-5 max-w-5xl mx-auto">
-			<Head>
-				<title>Projects - Jack Burgess</title>
-				<meta
-					name="description"
-					content="London-based software developer and CompSci undergrad"
-				/>
-			</Head>
-			<h1 className="font-display text-7xl gradientText pb-2">My Projects</h1>
-			<div className="grid grid-cols-2 py-4 gap-4 ">
-				{posts.map((post) => (
-					<Link
-						key={post.slug}
-						href="/projects/[slug]"
-						as={`/projects/${post.slug}`}
-					>
-						<a className="items-center flex flex-col ">
-							<h1 className=" md:text-4xl w-full bg-gray-300 text-center title rounded-t-lg duration-1000 flex flex-wrap flex-col">
-								{post.title}
-								<img
-									className="h-60 rounded-t-xl border-2 object-cover"
-									src={post.imageURL}
-								/>
-							</h1>
-						</a>
-					</Link>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="p-5 max-w-5xl mx-auto">
+      <Head>
+        <title>Projects - Jack Burgess</title>
+        <meta
+          name="description"
+          content="London-based software developer and CompSci undergrad"
+        />
+      </Head>
+      <h1 className="font-display text-7xl gradientText pb-2">My Projects</h1>
+      <div className="grid grid-cols-2 py-4 gap-4 ">
+        {posts.map((post) => (
+          <Link
+            key={post.slug}
+            href="/projects/[slug]"
+            as={`/projects/${post.slug}`}
+          >
+            <a className="items-center flex flex-col ">
+              <h1 className=" md:text-4xl w-full bg-gray-300 text-center title rounded-t-lg duration-1000 flex flex-wrap flex-col">
+                {post.title}
+                <img
+                  className="h-60 rounded-t-xl border-2 object-cover"
+                  src={post.imageURL}
+                />
+              </h1>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
