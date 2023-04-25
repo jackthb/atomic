ba9ec209c6cdd6bae9659363feba7495915d589c
jackthb <19895932+jackthb@users.noreply.github.com>
2023-04-25 22:56:04 +0100

    chore: add burger

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,20 +2,25 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { FaLightbulb, FaMoon } from "react-icons/fa";
+import { FaHamburger, FaMoon } from "react-icons/fa";
+import Menu from "react-burger-menu/lib/menus/slide";
 
 export default function Header() {
   const router = useRouter();
+  const [isMenuOpen, setMenuOpen] = useState(false);
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  useEffect(() => setMounted(true), []);
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
 
   if (!mounted) return null;
 
   return (
     <header>
-      <div className="py-0 px-5 sm:text-xl text-sm onLoad max-w-5xl m-auto h-28 flex items-center">
+      <div className="py-0 px-5 sm:text-xl text-sm onLoad max-w-5xl m-auto h-28 md:flex hidden items-center">
         <div
           className={`${
             router.pathname == "/" ? "hidden" : ""
@@ -61,6 +66,111 @@ export default function Header() {
             <FaMoon />
           </button>
         </nav>
+      </div>
+      <div className="sm:hidden">
+        <Menu
+          onOpen={() => setMenuOpen(true)}
+          onClose={() => setMenuOpen(false)}
+          isOpen={isMenuOpen}
+          right
+          styles={{
+            bmBurgerButton: {
+              position: "fixed",
+              width: "36px",
+              height: "30px",
+              right: "36px",
+              top: "36px",
+            },
+            bmBurgerBars: {
+              background: "#373a47",
+              borderRadius: "10px",
+            },
+            bmCrossButton: {
+              position: "fixed",
+              width: "36px",
+              height: "30px",
+              right: "36px",
+              top: "36px",
+            },
+            bmCross: {
+              background: "#bdc3c7",
+            },
+            bmMenuWrap: {
+              position: "fixed",
+              height: "100%",
+              width: "100%",
+            },
+            bmMenu: {
+              background: "#373a47",
+            },
+            bmMorphShape: {
+              fill: "#373a47",
+            },
+            bmOverlay: {
+              background: "rgba(0, 0, 0, 0.3)",
+              width: "100%",
+              height: "100%",
+              top: "0px",
+              right: "0px",
+              zIndex: "9999",
+            },
+          }}
+        >
+          <ul className="absolute grid w-full px-10 py-16">
+            <li className="border-b border-gray-300 py-5">
+              <Link
+                className="flex w-full font-semibold menu-item"
+                href="/"
+                onClick={() => setMenuOpen(false)}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="border-b border-gray-300 py-5">
+              <Link
+                className="flex w-full font-semibold menu-item"
+                href="/about"
+                onClick={() => setMenuOpen(false)}
+              >
+                About
+              </Link>
+            </li>
+            <li className="border-b border-gray-300 py-5">
+              <Link
+                className="flex w-full font-semibold menu-item"
+                href="/projects"
+                onClick={() => setMenuOpen(false)}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="border-b border-gray-300 py-5">
+              <Link
+                className="flex w-full font-semibold menu-item"
+                href="/blog"
+                onClick={() => setMenuOpen(false)}
+              >
+                Blog
+              </Link>
+            </li>
+            <li className="border-b border-gray-300 py-5">
+              <a
+                href="mailto:jack@sien.vision"
+                className="mr-4 hover:underline"
+              >
+                Contact
+              </a>
+            </li>
+            <li className="border-b border-gray-300 py-5">
+              <button
+                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                aria-label="Toggle theme"
+              >
+                <FaMoon />
+              </button>
+            </li>
+          </ul>
+        </Menu>
       </div>
     </header>
   );
