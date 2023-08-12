59845141089be3efb73d231d9fc97720eaa70b88
Jack Burgess <jack@ashbyhq.com>
2023-08-12 12:48:08 +0100

    chore: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import "../public/nprogress.css";
 import Layout from "../components/Layout";
 
 import NProgress from "nprogress";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
-import React from "react";
+import React, { useEffect } from "react";
+
+import * as gtag from "../lib/gtag";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -17,6 +19,17 @@ Router.events.on("routeChangeError", () => NProgress.done());
 export default function MyApp({ Component, pageProps }: AppProps) {
   const AnyComponent = Component as any;
   // wtf? Type error: 'Component' cannot be used as a JSX component.
+
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <ThemeProvider>
       <Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,8 @@ import Document, {
   NextScript,
   DocumentContext,
 } from "next/document";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -21,6 +23,15 @@ export default class MyDocument extends Document {
             name="og:image"
             content="https://res.cloudinary.com/du2zxsy39/image/upload/v1631560504/site_voffuw.png"
           />
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}');
+            `}
+          </Script>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
         </Head>
         <body>
           <Main />
